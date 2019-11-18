8bceb6d471d228cb99cdfdb21fc3f69b1ca2e6f6
StepanGavrilov <55409307+StepanGavrilov@users.noreply.github.com>
2019-11-19 01:50:34 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	
+	char Sequence;
+	string Check;
+
+
+	bool Attemp = true;
+	
+	while (Attemp == true) 
+	{
+		Sequence = getchar();
+		Check = Sequence;
+		
+		if (Check.length() > 500) 
+		{
+			cout << "Превышенно количество символов" << endl;
+		}
+		else if (Check.length() < 1) 
+		{
+			cout <<"Недостаточное количество символов" <<endl;
+		}
+		
+		
+
+		if (Check == ".")
+		{    
+			
+			cout << "Поставленна точка" << endl;
+			cout << Sequence << endl;
+			cout << Check << endl;
+			Attemp = false;
+			break;
+		}
+		else 
+		{
+			continue;
+
+		}
+	
+	
+	
+	}
+	
+	
+	
+	
+	return 0;
+}
