91bc0a680c2c41387f6be8118d5c36afb852ce3e
StepanGavrilov <55409307+StepanGavrilov@users.noreply.github.com>
2019-11-20 02:34:17 +0300

    Add files via upload

---
+++ Лаба4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	char arr[10][51];
+	int i = 0, j = 0;
+
+	char c = getchar();
+
+	while (c != '.') {
+		if (c == '.') {
+			//arr[i][j] = '\n';
+			i++;
+			j = 0;
+			while (c == ' ') c = getchar();
+
+		}
+		arr[i][j] = c;
+		c = getchar();
+		j++;
+		//cout << arr << endl;
+		cout << c << endl;
+
+
+	}
+	
+	return 0;
+}
